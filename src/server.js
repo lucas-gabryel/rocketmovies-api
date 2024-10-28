7beb3ad2e6fe5aa83fd15a429290a3983f0f68be
Lucas Gabryel <69809536+lucas-gabryel@users.noreply.github.com>
2024-10-28 11:06:51 -0300

    add sqlite and sqlite3 and create database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-
+const database = require('./database/sqlite');
 const AppError = require('./utils/AppError');
 
 const express = require('express');
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json()); // informa ao servidor que as requisições serão feitas em JSON
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next ) => {
     if(error instanceof AppError){
