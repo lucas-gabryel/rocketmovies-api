df6912c619cd9d30327d0ac3565b9794c8887c40
Lucas Gabryel <69809536+lucas-gabryel@users.noreply.github.com>
2024-10-28 09:54:18 -0300

    create routes for users

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(express.json()); // informa ao servidor que as requisições serão feitas em JSON
+
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
