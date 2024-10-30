304bd91ca3879b8d6da23497bb60a74da36e6d17
Lucas Gabryel <69809536+lucas-gabryel@users.noreply.github.com>
2024-10-30 18:51:21 -0300

    create method for actualization the username and email in database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,6 +2,7 @@ const { hash } = require('bcryptjs'); //importa a biblioteca de criptografia de 
 const AppError = require('../utils/AppError');
 
 const sqliteConnection = require('../database/sqlite');
+const { use } = require('express/lib/router');
 
 class UsersController {
     async create(request, response) {
@@ -20,6 +21,38 @@ class UsersController {
 
         return response.status(201).json(); // vai retornar um status de 201, significando que deu tudo certo e o user foi criado, retorna tbm um json vazio
 
+    }
+
+    async update(request, response) {
+        const { name, email, password} = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado");
+        }
+
+        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
     }
 }
 
