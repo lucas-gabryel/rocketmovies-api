74668e79769cd3e9418f11a78a7d6e6a72c74f4b
Lucas Gabryel <69809536+lucas-gabryel@users.noreply.github.com>
2024-11-03 18:26:16 -0300

    feat: add tags searches in async index

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -46,12 +46,22 @@ class NotesController{
     }
 
     async index(request, response){
-        const { user_id, title } = request.query;
+        const { user_id, title, tags } = request.query;
 
-        const notes = await knex("movie_notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`) // fazer pesquisas de notas que contenham um certo nome digitado pelo usuário, as porcentagens removem os espaços em branco
-        .orderBy("title");
+        let notes;
+
+        if(tags) {
+            const filterTags = tags.split(",").map(tag => tag.trim());
+
+            notes = await knex("tags")
+            .whereIn("name", filterTags)
+            
+        } else {
+            notes = await knex("movie_notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`) // fazer pesquisas de notas que contenham um certo nome digitado pelo usuário, as porcentagens removem os espaços em branco
+            .orderBy("title");
+        }
 
         return response.json(notes);
     }
