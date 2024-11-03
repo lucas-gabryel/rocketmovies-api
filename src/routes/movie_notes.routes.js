439416b12a5a34db6346386ce9a0cce706d19473
Lucas Gabryel <69809536+lucas-gabryel@users.noreply.github.com>
2024-11-03 17:43:39 -0300

    feat: add async function show in NotesController

--- src/routes/movie_notes.routes.js
+++ src/routes/movie_notes.routes.js
@@ -7,5 +7,6 @@ const notesRoutes = Router();
 const movieNotesController = new MovieNotesController();
 
 notesRoutes.post('/:user_id', movieNotesController.create);
+notesRoutes.get('/:id', movieNotesController.show);
 
 module.exports = notesRoutes;
